cc292687198b34b82c698cff5dedbaf40ccb6827
wjwj910 <wjwj910@gmail.com>
2025-03-19 11:32:05 +0900

    first commit

---
+++ src/main/kotlin/com/back/Kp20250319Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kp20250319Application
+
+fun main(args: Array<String>) {
+    runApplication<Kp20250319Application>(*args)
+}
